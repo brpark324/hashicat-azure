4c32fc7bcc9b0c620f315f5c6cd7a47459fbcbf1
brpark <lb.khat2020@gmail.com>
2020-11-27 07:27:12 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/cloocus-brpark/network/azurerm"
   version = "3.0.1"
-  resource_group_name = "brpark"
+  resource_group_name = "${var.prefix}-workshop"
   # insert required variables here
 }
